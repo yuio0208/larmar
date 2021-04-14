356c3be695d35b35700fa4d82b8c9d7d3b62c257
Your Name <yuio0208@example.com>
2021-04-14 10:41:10 +0900

    test2

--- src/wkqk/TEST1.java
+++ src/wkqk/TEST1.java
@@ -7,7 +7,7 @@ public class TEST1 {
 	public static void main9(String[] args) {
 		// TODO Auto-generated method stub
 		String abc = "123";
-		String cdf = "456";
+		String cdf = "45678";
 		System.out.println(abc+cdf);
 		int k = Integer.parseInt(abc);
 		int kk = Integer.parseInt(cdf);
@@ -17,17 +17,7 @@ public class TEST1 {
 		char b = 'a';
 		int c = (int)b;
 		System.out.println(c);
-		
-		
-		
-						
-					
-		
-		
-		
-		
-		
-		
+	
 		
 	}
 
